4cad8c4a8bf6d8828e6d4a54d3471265cb587b5e
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-10-17 00:56:04 +0200

    hack version file

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
+import os
 
+# fuck distutils2
+version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
+with open(version_file, "r") as fh:
+    __version__ = fh.read().rstrip('\n')
 
 with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="timetools", # Replace with your own username
-    version="0.1",
+    version=__version__,
     author="Maximilien Lehujeur",
     author_email="maximilien.lehujeur@gmail.com",
     description="",
---
+++ timetools/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+# fuck distutils2
+version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
+with open(version_file, "r") as fh:
+    __version__ = fh.read().rstrip('\n')
