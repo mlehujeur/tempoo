b04424dffb854ff98870ff4447ce1425d141857c
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-10-17 00:59:08 +0200

    fix version file bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 import os
 
 # fuck distutils2
-version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
+version_file = os.path.join('timetools', 'version.txt')
 with open(version_file, "r") as fh:
     __version__ = fh.read().rstrip('\n')
 
