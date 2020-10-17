9c91c3e3b8c4dc70d32ecc8ccb685aed19f44f3c
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-10-17 09:08:05 +0200

    upd __init__

--- timetools/__init__.py
+++ timetools/__init__.py
@@ -4,3 +4,6 @@ import os
 version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
 with open(version_file, "r") as fh:
     __version__ = fh.read().rstrip('\n')
+
+from timetools.utc import UTC, UTCFromTimestamp, UTCFromStr
+from timetools.timetick import timetick
