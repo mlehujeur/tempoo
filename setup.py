a1db1f3a55b997af3bb5b527c0468bb534c6ed45
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-10-17 13:27:21 +0200

    need python file for version

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@ import setuptools
 import os
 
 # fuck distutils2
-version_file = os.path.join('timetools', 'version.txt')
-with open(version_file, "r") as fh:
-    __version__ = fh.read().rstrip('\n')
+version_file = os.path.join('timetools', 'version.py')
+if not os.path.isfile(version_file):
+    raise IOError(version_file)
+
+with open(version_file, "r") as fid:
+    for l in fid:
+        if l.strip('\n').strip().startswith('__version__'):
+            __version__ = l.strip('\n').split('=')[-1].split()[0].strip()
+            break
+    else:
+        raise Exception(f'could not detect __version__ affectation in {version_file}')
+
 
 with open("Readme.md", "r") as fh:
     long_description = fh.read()
--- timetools/__init__.py
+++ timetools/__init__.py
@@ -1,9 +1,3 @@
-import os
-
-# fuck distutils2
-version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
-with open(version_file, "r") as fh:
-    __version__ = fh.read().rstrip('\n')
-
+from timetools.version import __version__
 from timetools.utc import UTC, UTCFromTimestamp, UTCFromStr
 from timetools.timetick import timetick
