c60ae7b24a7ab2304052c88f9b9037604b2fece6
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2021-01-31 20:36:12 +0100

    fix python_requires, must be exactly 3.7

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: Linux"],
-    python_requires='3.7',
+    python_requires='>=3.7, <3.8',
     install_requires=[
         'numpy', 'matplotlib', 'pytest'],
     scripts=["timetools/bin/doy"])
