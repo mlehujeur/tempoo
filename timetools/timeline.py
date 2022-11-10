87d829a31629033d96e823513481f2c01412a9c4
mlehujeur <maximilien.lehujeur@gmail.com>
2022-11-10 11:56:40 +0100

    improve timeline

--- timetools/timeline.py
+++ timetools/timeline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys, glob, os, time
+import curses
 import numpy as np
 import matplotlib.pyplot as plt
 from timetools.utc import UTCFromStr, UTCFromTimestamp
@@ -9,33 +10,50 @@ from timetools.timetick import timetick
 if __name__ == "__main__":
 
     timeline_file = sys.argv[1]
-    now = UTCFromTimestamp(time.time())
 
-    ax = plt.gca()
-    with open(timeline_file, 'r') as fid:
-        for n, l in enumerate(fid):
-            if l.startswith('#'):
-                continue
-            if "," not in l:
-                continue
-            start, end, title = l.split('\n')[0].split(',')
+    while True:
+        try:
+            now = UTCFromTimestamp(time.time())
+            fig = plt.figure(figsize=(18, 4))
+            fig.subplots_adjust(left=0.025, right=1. - 0.01)
+            ax = plt.gca()
 
-            start = UTCFromStr(start)                        
-            end = UTCFromStr(end)
+            with open(timeline_file, 'r') as fid:
+                for n, l in enumerate(fid):
+                    if l.startswith('#'):
+                        continue
+                    if "," not in l:
+                        continue
+                    linenumber, rgb, start, end, title = l.split('\n')[0].split(',')
+                    linenumber = int(linenumber)
+                    r, g, b = np.asarray(rgb.split('/'), int) / 255.
+                    try:
+                        start = UTCFromStr(start)                        
+                        end = UTCFromStr(end)
+                    except:
+                        raise Exception(start, end)
 
-            hdl, = ax.plot(
-                [start.timestamp, end.timestamp],
-                [n, n],
-                linewidth=3)
-            ax.text(
-                start.timestamp,
-                n, title,
-                ha="left", 
-                va="bottom",
-                color=hdl.get_color())
-    ylim = ax.get_ylim()
-    ax.plot(now.timestamp * np.ones(2), ylim, 'r--')
-    ax.grid(True)
-    timetick(plt.gca())
-    plt.show()
+                    hdl, = ax.plot(
+                        [start.timestamp, end.timestamp],
+                        [linenumber, linenumber],
+                        linewidth=3,
+                        color=[r, g, b])
+                    ax.text(
+                        start.timestamp,
+                        linenumber, title,
+                        ha="left", 
+                        va="bottom",
+                        color=hdl.get_color())
+            ylim = ax.get_ylim()
+            ax.plot(now.timestamp * np.ones(2), ylim, 'r--')
+            ax.grid(True)
+            timetick(plt.gca())
+            plt.ion()
+            plt.show()
+            ans = input('q=quit, else redraw')
 
+            if ans.lower() == "q":
+                break
+            plt.close(fig)
+        except KeyboardInterrupt:
+            sys.exit(1)
